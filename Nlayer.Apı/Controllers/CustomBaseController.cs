577168fc97e23f69aa687c15365e126375837be5
sezin salli <sezininyayla@gmail.com>
2023-05-03 16:23:58 +0300

    controller

---
+++ Nlayer.Apı/Controllers/CustomBaseController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Nlayer.Core.DTOs;
+
+namespace Nlayer.Apı.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomBaseController : ControllerBase
+    {
+        
+    }
+}
--- Nlayer.Apı/Controllers/ProductsController.cs
+++ Nlayer.Apı/Controllers/ProductsController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Nlayer.Core.DTOs;
+using Nlayer.Core.Entity;
+using Nlayer.Core.Service;
 
 namespace Nlayer.Apı.Controllers
 {
@@ -7,5 +11,22 @@ namespace Nlayer.Apı.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
+        private readonly IMapper _mapper;
+        private readonly IService<Product> _service;
+        public ProductsController(IMapper mapper, IService<Product> service)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+        
+        public async Task<IActionResult> All()
+        {
+            var products=await _service.GetAllAsync();
+            var productsDtos=_mapper.Map<List<ProductDto>>(products.ToList());
+
+            return Ok(CustomResponseDto<List<ProductDto>>.Success(200,productsDtos));
+        }
+
+
     }
 }
