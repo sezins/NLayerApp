ba7e3e010c5cb259bfcdf712ae068431a1ad2f78
sezin salli <sezininyayla@gmail.com>
2023-05-15 11:58:18 +0300

    updated

---
+++ Nlayer.Apı/Middlewares/UseCustomExceptionHandler.cs
@@ -0,0 +1,16 @@
+﻿namespace Nlayer.Apı.Middlewares
+{
+    public static class UseCustomExceptionHandler
+    {
+        public static void UserCustomException(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(config =>
+            {
+                config.Run(async context => 
+                {
+                    context.Response.ContentType = "applicaiton/json";
+                })
+            })
+        }
+    }
+}
---
+++ Nlayer.Repository/Migrations/20230515064501_mig1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Nlayer.Repository.Migrations
+{
+    public partial class mig1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2023, 5, 15, 9, 45, 1, 307, DateTimeKind.Local).AddTicks(6340));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2023, 5, 15, 9, 45, 1, 307, DateTimeKind.Local).AddTicks(6349));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2023, 5, 15, 9, 45, 1, 307, DateTimeKind.Local).AddTicks(6351));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2023, 5, 3, 9, 44, 39, 765, DateTimeKind.Local).AddTicks(1798));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2023, 5, 3, 9, 44, 39, 765, DateTimeKind.Local).AddTicks(1809));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2023, 5, 3, 9, 44, 39, 765, DateTimeKind.Local).AddTicks(1810));
+        }
+    }
+}
